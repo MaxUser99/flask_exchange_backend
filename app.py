2eeef0bc32c0cae4b73df259b0fce99b1fbaa9f7
maxim <m.ossas@cleverdec.com>
2019-05-14 16:19:32 +0300

    added CORS support

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 import sys
-from flask import Flask
-from flask_mongoengine import MongoEngine
+
 from cerberus import Validator
+from flask import Flask, jsonify
+from flask_mongoengine import MongoEngine
 
 validator = Validator()
 sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '../../')))
@@ -12,6 +13,14 @@ app.config['MONGO_DBNAME'] = 'users'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/users'
 db = MongoEngine()
 db.init_app(app)
+
+
+def CORSify(resp):
+    if type(resp) is dict:
+        resp = jsonify(resp)
+    resp.headers.add("Access-Control-Allow-Origin", "*")
+    return resp
+
 
 from endpoints import *
 
